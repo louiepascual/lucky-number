c9bb3959f439a63f68c6ebcfbd57272de97a81f2
Louie Pascual <louieepascual@gmail.com>
2024-01-30 16:52:43 +0800

    init repo
    Signed-off-by: Louie Pascual <louieepascual@gmail.com>

---
+++ cmd/cli/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/louiepascual/lucky-number/internal/random"
+
+	"github.com/fatih/color"
+
+)
+
+func main() {
+	green := color.New(color.FgGreen)
+
+	fmt.Printf("Your lucky number is ")
+	green.Printf("%d!\n", random.Number())
+
+
+
+}
